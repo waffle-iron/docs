6608f95977b14f291a5757a584598bac97063d4b
Andrew Stevenson <andrew@datamountaineer.com>
2016-04-19 00:11:47 +0200

    initial commit rtd

--- source/conf.py
+++ source/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'StreamReactor'
-copyright = u'2016, Andrew Stevenson'
+copyright = u'2016, DataMountaineer'
 author = u'Andrew Stevenson'
 
 # The version info for the project you're documenting, acts as replacement for
